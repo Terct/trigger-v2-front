dd6643b067cff54399b473a7a946f2ab8a52de1a
Armando Castro <armandocastrodesousa@gmail.com>
2024-02-28 21:19:41 +0000

    fisrt commit

--- pages/Configs/jsobjects/Functions/Functions.js
+++ pages/Configs/jsobjects/Functions/Functions.js
@@ -20,7 +20,7 @@ export default {
 					"start": InputTriggerForListStart.text,
 					"end": InputTriggerForListEnd.text
 				},
-				"daysOfWeeks": CheckboxGroupTriggerForEvents.selectedValues
+				"daysOfWeeks": CheckboxGroupTriggerForList.selectedValues
 			}
 		}
 
@@ -30,18 +30,52 @@ export default {
 
 		request_updateOpeningHours.run(() => {
 			if (request_updateOpeningHours.data.message === 'Opening_hours atualizadas com sucesso') {
-				
+
 				showAlert("Horário de funcionamento atualizado com sucesso.", 'success');
 				// Faça algo após a remoção bem-sucedida, se necessário.
 			} else {
-				showAlert("Erro ao modificar a setar horario de funcionamento", 'error');
+				showAlert("Erro ao modificar o horario de funcionamento", 'error');
 			}
 		}, (error) => {
 
 			console.error('Erro durante a execução do request_updateOpeningHours:', error);
-			showAlert("Erro ao modificar a setar horario de funcionamento", 'error');
+			showAlert("Erro ao modificar o horario de funcionamento", 'error');
 		});
 
+
+	},
+
+
+
+	async search_OpeningHours(){
+
+		request_getOpeningHours.run(() => {
+
+			if (request_getOpeningHours.data) {
+
+				SwitchTriggerForEvents.setValue(request_getOpeningHours.data.triggerForEvents.status)
+
+				InputTriggerForEventsStart.setValue(request_getOpeningHours.data.triggerForEvents.timesOfDay.start)
+				InputTriggerForEventsEnd.setValue(request_getOpeningHours.data.triggerForEvents.timesOfDay.end)
+
+				CheckboxGroupTriggerForEvents.setSelectedOptions(request_getOpeningHours.data.triggerForEvents.daysOfWeeks)
+
+
+				SwitchTriggerForList.setValue(request_getOpeningHours.data.triggerForList.status)
+
+				InputTriggerForListStart.setValue(request_getOpeningHours.data.triggerForList.timesOfDay.start)
+				InputTriggerForListEnd.setValue(request_getOpeningHours.data.triggerForList.timesOfDay.end)
+
+				CheckboxGroupTriggerForList.setSelectedOptions(request_getOpeningHours.data.triggerForList.daysOfWeeks)
+
+			} else {
+				showAlert("Erro ao buscar horario de funcionamento", 'error');
+			}
+		}, (error) => {
+
+			console.error('Erro durante a execução do request_getOpeningHours:', error);
+			showAlert("Erro ao buscar horario de funcionamento", 'error');
+		});
 
 
 
