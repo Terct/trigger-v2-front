25b09567e0d51153d9164b4bce30a0443bca1959
Armando Castro <armandocastrodesousa@gmail.com>
2024-02-28 20:27:44 +0000

    first commit

--- pages/Configs/jsobjects/Functions/Functions.js
+++ pages/Configs/jsobjects/Functions/Functions.js
@@ -1,6 +1,47 @@
 export default {
 
+	data: [],
+
 	async setOpeningHours(){
+
+
+		var data = {
+			"triggerForEvents": {
+				"status": SwitchTriggerForEvents.value,
+				"timesOfDay": {
+					"start": InputTriggerForEventsStart.text,
+					"end": InputTriggerForEventsEnd.text
+				},
+				"daysOfWeeks": CheckboxGroupTriggerForEvents.selectedValues
+			},
+			"triggerForList": {
+				"status": SwitchTriggerForList.value,
+				"timesOfDay": {
+					"start": InputTriggerForListStart.text,
+					"end": InputTriggerForListEnd.text
+				},
+				"daysOfWeeks": CheckboxGroupTriggerForEvents.selectedValues
+			}
+		}
+
+		//console.log(data)
+		this.data = data
+
+
+		request_updateOpeningHours.run(() => {
+			if (request_updateOpeningHours.data.message === 'Opening_hours atualizadas com sucesso') {
+				
+				showAlert("Horário de funcionamento atualizado com sucesso.", 'success');
+				// Faça algo após a remoção bem-sucedida, se necessário.
+			} else {
+				showAlert("Erro ao modificar a setar horario de funcionamento", 'error');
+			}
+		}, (error) => {
+
+			console.error('Erro durante a execução do request_updateOpeningHours:', error);
+			showAlert("Erro ao modificar a setar horario de funcionamento", 'error');
+		});
+
 
 
 
