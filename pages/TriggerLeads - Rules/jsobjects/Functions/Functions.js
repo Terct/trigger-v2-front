7ba2dca15d97cc2f4516669a805ca08ca7ec9d52
Armando Castro <armandocastrodesousa@gmail.com>
2024-04-26 18:21:02 +0000

    first commit

--- pages/TriggerLeads - Rules/jsobjects/Functions/Functions.js
+++ pages/TriggerLeads - Rules/jsobjects/Functions/Functions.js
@@ -3,5 +3,18 @@ export default {
 	data: [],
 
 
+	async preencherFormula(combine, newFormula){
+
+		var oldFormula = InpCondicion_Rules.text
+
+		if (oldFormula.length === 0){
+			InpCondicion_Rules.setValue(newFormula)
+		}
+		else{
+			InpCondicion_Rules.setValue( oldFormula + " " + combine + " " + newFormula )
+		}
+	}
+
+
 
 }
